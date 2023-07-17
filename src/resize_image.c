32cb00f0598cbba1c48899fa645d092070bd3020
i am centered <khushgupta22@iitk.ac.in>
2023-07-17 11:16:47 +0530

    codespaces

--- src/resize_image.c
+++ src/resize_image.c
@@ -4,13 +4,16 @@
 float nn_interpolate(image im, float x, float y, int c)
 {
     // TODO Fill in
-    return 0;
+    int nw_x = (int)roundf(x);
+    int nw_y = (int)roundf(y);
+
+    return get_pixel(im, nw_x, nw_y, c);
 }
 
 image nn_resize(image im, int w, int h)
 {
     // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    return resized;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
