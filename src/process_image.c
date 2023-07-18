97654bd0abbf0d5665be2448ea7a28e03c4b79be
Khush <khushgupta711@gmail.com>
2023-07-18 22:24:29 +0530

    Assignment 2 completed

--- src/process_image.c
+++ src/process_image.c
@@ -7,22 +7,28 @@
 float get_pixel(image im, int x, int y, int c)
 {
     // TODO Fill this in
-    if (x < 0 || x >= im.w){
+    if (x < 0 || x >= im.w)
+    {
         x = (x < 0) ? 0 : im.w - 1;
     }
-    else{
+    else
+    {
         x = x;
     }
-    if (y < 0 || y >= im.h){
+    if (y < 0 || y >= im.h)
+    {
         y = (y < 0) ? 0 : im.h - 1;
     }
-    else{
+    else
+    {
         y = y;
     }
-    if (c < 0 || c >= im.c){
+    if (c < 0 || c >= im.c)
+    {
         c = (c < 0) ? 0 : im.c - 1;
     }
-    else{
+    else
+    {
         c = c;
     }
     int value = x + y * im.w + c * im.w * im.h;
@@ -32,11 +38,13 @@ float get_pixel(image im, int x, int y, int c)
 void set_pixel(image im, int x, int y, int c, float v)
 {
     // TODO Fill this in
-    if (x >= 0 && x < im.w && y >= 0 && y < im.h && c >= 0 && c < im.c){
+    if (x >= 0 && x < im.w && y >= 0 && y < im.h && c >= 0 && c < im.c)
+    {
         int value = (x + y * im.w + c * im.w * im.h);
         im.data[value] = v;
     }
-    else{
+    else
+    {
         if (x < 0 || x >= im.w)
             x = (x < 0) ? 0 : im.w - 1;
         if (y < 0 || y >= im.h)
@@ -52,15 +60,18 @@ image copy_image(image im)
 {
     image copy = make_image(im.w, im.h, im.c);
     // TODO Fill this in
-    for (int i = 0; i < im.c;i++){
-        for (int j = 0; j < im.h;j++){
-            for (int k = 0; k < im.w;k++){
+    for (int i = 0; i < im.c; i++)
+    {
+        for (int j = 0; j < im.h; j++)
+        {
+            for (int k = 0; k < im.w; k++)
+            {
                 float v = get_pixel(im, k, j, i);
                 set_pixel(copy, k, j, i, v);
             }
         }
     }
-        return copy;
+    return copy;
 }
 
 image rgb_to_grayscale(image im)
@@ -68,8 +79,10 @@ image rgb_to_grayscale(image im)
     assert(im.c == 3);
     image gray = make_image(im.w, im.h, 1);
     // TODO Fill this in
-    for (int i = 0; i < im.h; i++){
-        for (int j = 0; j < im.w; j++){
+    for (int i = 0; i < im.h; i++)
+    {
+        for (int j = 0; j < im.w; j++)
+        {
             float r = get_pixel(im, j, i, 0);
             float g = get_pixel(im, j, i, 1);
             float b = get_pixel(im, j, i, 2);
@@ -84,120 +97,147 @@ image rgb_to_grayscale(image im)
         }
     }
 
-        return gray;
+    return gray;
 }
 
 void shift_image(image im, int c, float v)
 {
     // TODO Fill this in
-        for (int i = 0; i < im.h;i++){
-            for (int j = 0; j < im.w;j++){
+    for (int i = 0; i < im.h; i++)
+    {
+        for (int j = 0; j < im.w; j++)
+        {
             float value = get_pixel(im, j, i, c);
             float shift = value + v;
             set_pixel(im, j, i, c, shift);
-            }
         }
+    }
 }
 
-void clamp_image(image im){
+void clamp_image(image im)
+{
     // TODO Fill this in
-        for (int i = 0; i < im.c; i++){
-            for (int j = 0; j < im.h; j++){
-            for (int k = 0; k < im.w; k++){
+    for (int i = 0; i < im.c; i++)
+    {
+        for (int j = 0; j < im.h; j++)
+        {
+            for (int k = 0; k < im.w; k++)
+            {
                 float v = get_pixel(im, k, j, i);
                 float new_value = (v < 0) ? 0 : (v > 1 ? 1 : v);
                 set_pixel(im, k, j, i, new_value);
             }
-            }
         }
+    }
 }
-            // These might be handy
-float three_way_max(float a, float b, float c){
-                return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-            }
+// These might be handy
+float three_way_max(float a, float b, float c)
+{
+    return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+}
 
-float three_way_min(float a, float b, float c){
-                return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
-            }
+float three_way_min(float a, float b, float c)
+{
+    return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+}
 
-void rgb_to_hsv(image im){
+void rgb_to_hsv(image im)
+{
     // TODO Fill this in
-    for (int i=0; i < im.h; i++){
-        for (int j=0; j < im.w;j++){
+    for (int i = 0; i < im.h; i++)
+    {
+        for (int j = 0; j < im.w; j++)
+        {
             float r = get_pixel(im, j, i, 0);
             float g = get_pixel(im, j, i, 1);
             float b = get_pixel(im, j, i, 2);
             float V = three_way_max(r, g, b);
             float m = three_way_min(r, g, b);
             float C = V - m;
-            float S,H,Hdash;
-            if(V==0){
+            float S, H, Hdash;
+            if (V == 0)
+            {
                 S = 0;
             }
-            else{
+            else
+            {
                 S = C / V;
             }
-            if(C!=0){
-                if(V==r)
+            if (C != 0)
+            {
+                if (V == r)
                     Hdash = (g - b) / C;
-                else if(V==g)
+                else if (V == g)
                     Hdash = (b - r) / C + 2;
-                else if(V==b)
+                else if (V == b)
                     Hdash = (r - g) / C + 4;
                 H = (Hdash < 0) ? (Hdash / 6 + 1) : (Hdash / 6);
             }
-            else if(C==0){
+            else if (C == 0)
+            {
                 H = 0;
-            } 
+            }
             set_pixel(im, j, i, 0, H);
             set_pixel(im, j, i, 1, S);
             set_pixel(im, j, i, 2, V);
-         }
+        }
     }
 }
-void hsv_to_rgb(image im){
+void hsv_to_rgb(image im)
+{
     // TODO Fill this in
-    for (int i = 0; i < im.h;i++){
-         for (int j = 0; j < im.w;j++){
+    for (int i = 0; i < im.h; i++)
+    {
+        for (int j = 0; j < im.w; j++)
+        {
             float h = get_pixel(im, j, i, 0);
             float s = get_pixel(im, j, i, 1);
             float v = get_pixel(im, j, i, 2);
-            float r, g, b,Hdash,C;
-            if(v==0){
+            float r, g, b, Hdash, C;
+            if (v == 0)
+            {
                 r = g = b = 0;
             }
-            else if(h==0){
+            else if (h == 0)
+            {
                 r = g = b = v;
             }
-            else{
-                Hdash = h * 6;
+            else
+            {
+                Hdash = h * 6.0;
                 C = s * v;
-                if (Hdash < 1){
+                if (Hdash < 1)
+                {
                     r = v;
                     g = Hdash * C + v - C;
                     b = v - C;
                 }
-                else if (Hdash < 2){
+                else if (Hdash < 2)
+                {
                     r = (2 - Hdash) * C + v - C;
                     g = v;
                     b = v - C;
                 }
-                else if (Hdash < 3){
+                else if (Hdash < 3)
+                {
                     r = v - C;
                     g = v;
                     b = (Hdash - 2) * C + v - C;
                 }
-                else if (Hdash < 4){
+                else if (Hdash < 4)
+                {
                     r = v - C;
                     g = (4 - Hdash) * C + v - C;
                     b = v;
                 }
-                else if (Hdash < 5){
+                else if (Hdash < 5)
+                {
                     r = (Hdash - 4) * C + v - C;
                     g = v - C;
                     b = v;
                 }
-                else{
+                else
+                {
                     r = v;
                     g = v - C;
                     b = (6 - Hdash) * C + v - C;
@@ -206,6 +246,6 @@ void hsv_to_rgb(image im){
             set_pixel(im, j, i, 0, r);
             set_pixel(im, j, i, 1, g);
             set_pixel(im, j, i, 2, b);
-         }
+        }
     }
 }
--- src/test.c
+++ src/test.c
@@ -9,53 +9,75 @@
 int tests_total = 0;
 int tests_fail = 0;
 
-int within_eps(float a, float b){
-    return a-EPS<b && b<a+EPS;
+int within_eps(float a, float b)
+{
+    return a - EPS < b && b < a + EPS;
 }
 
-int same_image(image a, image b){
+int same_image(image a, image b)
+{
     int i;
-    if(a.w != b.w || a.h != b.h || a.c != b.c) {
+    if (a.w != b.w || a.h != b.h || a.c != b.c)
+    {
         printf("Expected %d x %d x %d image, got %d x %d x %d\n", b.w, b.h, b.c, a.w, a.h, a.c);
         return 0;
     }
-    for(i = 0; i < a.w*a.h*a.c; ++i){
-        if(!within_eps(a.data[i], b.data[i])) 
+    for (i = 0; i < a.w * a.h * a.c; ++i)
+    {
+        if (!within_eps(a.data[i], b.data[i]))
         {
-            printf("The value should be %f, but it is %f! \n", b.data[i], a.data[i]);
+            printf("The value should be %f, but it is %f, i:%d! \n", b.data[i], a.data[i], i);
             return 0;
         }
     }
     return 1;
 }
 
-void test_get_pixel(){
+void test_get_pixel()
+{
     image im = load_image("data/dots.png");
     // Test within image
-    TEST(within_eps(0, get_pixel(im, 0,0,0)));
-    TEST(within_eps(1, get_pixel(im, 1,0,1)));
-    TEST(within_eps(0, get_pixel(im, 2,0,1)));
+    TEST(within_eps(0, get_pixel(im, 0, 0, 0)));
+    TEST(within_eps(1, get_pixel(im, 1, 0, 1)));
+    TEST(within_eps(0, get_pixel(im, 2, 0, 1)));
 
     // Test padding
-    TEST(within_eps(1, get_pixel(im, 0,3,1)));
-    TEST(within_eps(1, get_pixel(im, 7,8,0)));
-    TEST(within_eps(0, get_pixel(im, 7,8,1)));
-    TEST(within_eps(1, get_pixel(im, 7,8,2)));
+    TEST(within_eps(1, get_pixel(im, 0, 3, 1)));
+    TEST(within_eps(1, get_pixel(im, 7, 8, 0)));
+    TEST(within_eps(0, get_pixel(im, 7, 8, 1)));
+    TEST(within_eps(1, get_pixel(im, 7, 8, 2)));
     free_image(im);
 }
 
-void test_set_pixel(){
+void test_set_pixel()
+{
     image gt = load_image("data/dots.png");
-    image d = make_image(4,2,3);
-    set_pixel(d, 0,0,0,0); set_pixel(d, 0,0,1,0); set_pixel(d, 0,0,2,0); 
-    set_pixel(d, 1,0,0,1); set_pixel(d, 1,0,1,1); set_pixel(d, 1,0,2,1); 
-    set_pixel(d, 2,0,0,1); set_pixel(d, 2,0,1,0); set_pixel(d, 2,0,2,0); 
-    set_pixel(d, 3,0,0,1); set_pixel(d, 3,0,1,1); set_pixel(d, 3,0,2,0); 
+    image d = make_image(4, 2, 3);
+    set_pixel(d, 0, 0, 0, 0);
+    set_pixel(d, 0, 0, 1, 0);
+    set_pixel(d, 0, 0, 2, 0);
+    set_pixel(d, 1, 0, 0, 1);
+    set_pixel(d, 1, 0, 1, 1);
+    set_pixel(d, 1, 0, 2, 1);
+    set_pixel(d, 2, 0, 0, 1);
+    set_pixel(d, 2, 0, 1, 0);
+    set_pixel(d, 2, 0, 2, 0);
+    set_pixel(d, 3, 0, 0, 1);
+    set_pixel(d, 3, 0, 1, 1);
+    set_pixel(d, 3, 0, 2, 0);
 
-    set_pixel(d, 0,1,0,0); set_pixel(d, 0,1,1,1); set_pixel(d, 0,1,2,0); 
-    set_pixel(d, 1,1,0,0); set_pixel(d, 1,1,1,1); set_pixel(d, 1,1,2,1); 
-    set_pixel(d, 2,1,0,0); set_pixel(d, 2,1,1,0); set_pixel(d, 2,1,2,1); 
-    set_pixel(d, 3,1,0,1); set_pixel(d, 3,1,1,0); set_pixel(d, 3,1,2,1); 
+    set_pixel(d, 0, 1, 0, 0);
+    set_pixel(d, 0, 1, 1, 1);
+    set_pixel(d, 0, 1, 2, 0);
+    set_pixel(d, 1, 1, 0, 0);
+    set_pixel(d, 1, 1, 1, 1);
+    set_pixel(d, 1, 1, 2, 1);
+    set_pixel(d, 2, 1, 0, 0);
+    set_pixel(d, 2, 1, 1, 0);
+    set_pixel(d, 2, 1, 2, 1);
+    set_pixel(d, 3, 1, 0, 1);
+    set_pixel(d, 3, 1, 1, 0);
+    set_pixel(d, 3, 1, 2, 1);
 
     // Test images are same
     TEST(same_image(d, gt));
@@ -89,9 +111,9 @@ void test_shift()
     image c = copy_image(im);
     shift_image(c, 1, .1);
     TEST(within_eps(c.data[0], im.data[0]));
-    TEST(within_eps(c.data[im.w*im.h + 13], im.data[im.w*im.h+13] + .1));
-    TEST(within_eps(c.data[2*im.w*im.h + 72], im.data[2*im.w*im.h+72]));
-    TEST(within_eps(c.data[im.w*im.h + 47], im.data[im.w*im.h+47] + .1));
+    TEST(within_eps(c.data[im.w * im.h + 13], im.data[im.w * im.h + 13] + .1));
+    TEST(within_eps(c.data[2 * im.w * im.h + 72], im.data[2 * im.w * im.h + 72]));
+    TEST(within_eps(c.data[im.w * im.h + 47], im.data[im.w * im.h + 47] + .1));
     free_image(im);
     free_image(c);
 }
@@ -120,7 +142,7 @@ void test_hsv_to_rgb()
 void test_nn_resize()
 {
     image im = load_image("data/dogsmall.jpg");
-    image resized = nn_resize(im, im.w*4, im.h*4);
+    image resized = nn_resize(im, im.w * 4, im.h * 4);
     image gt = load_image("figs/dog4x-nn-for-test.png");
     TEST(same_image(resized, gt));
     free_image(im);
@@ -139,7 +161,7 @@ void test_nn_resize()
 void test_bl_resize()
 {
     image im = load_image("data/dogsmall.jpg");
-    image resized = bilinear_resize(im, im.w*4, im.h*4);
+    image resized = bilinear_resize(im, im.w * 4, im.h * 4);
     image gt = load_image("figs/dog4x-bl.png");
     TEST(same_image(resized, gt));
     free_image(im);
@@ -159,9 +181,10 @@ void test_multiple_resize()
 {
     image im = load_image("data/dog.jpg");
     int i;
-    for (i = 0; i < 10; i++){
-        image im1 = bilinear_resize(im, im.w*4, im.h*4);
-        image im2 = bilinear_resize(im1, im1.w/4, im1.h/4);
+    for (i = 0; i < 10; i++)
+    {
+        image im1 = bilinear_resize(im, im.w * 4, im.h * 4);
+        image im2 = bilinear_resize(im1, im1.w / 4, im1.h / 4);
         free_image(im);
         free_image(im1);
         im = im2;
@@ -172,14 +195,13 @@ void test_multiple_resize()
     free_image(gt);
 }
 
-
-void test_highpass_filter(){
+void test_highpass_filter()
+{
     image im = load_image("data/dog.jpg");
     image f = make_highpass_filter();
     image blur = convolve_image(im, f, 0);
     clamp_image(blur);
 
-    
     image gt = load_image("figs/dog-highpass.png");
     TEST(same_image(blur, gt));
     free_image(im);
@@ -188,13 +210,13 @@ void test_highpass_filter(){
     free_image(gt);
 }
 
-void test_emboss_filter(){
+void test_emboss_filter()
+{
     image im = load_image("data/dog.jpg");
     image f = make_emboss_filter();
     image blur = convolve_image(im, f, 1);
     clamp_image(blur);
 
-    
     image gt = load_image("figs/dog-emboss.png");
     TEST(same_image(blur, gt));
     free_image(im);
@@ -203,12 +225,12 @@ void test_emboss_filter(){
     free_image(gt);
 }
 
-void test_sharpen_filter(){
+void test_sharpen_filter()
+{
     image im = load_image("data/dog.jpg");
     image f = make_sharpen_filter();
     image blur = convolve_image(im, f, 1);
     clamp_image(blur);
-
 
     image gt = load_image("figs/dog-sharpen.png");
     TEST(same_image(blur, gt));
@@ -218,7 +240,8 @@ void test_sharpen_filter(){
     free_image(gt);
 }
 
-void test_convolution(){
+void test_convolution()
+{
     image im = load_image("data/dog.jpg");
     image f = make_box_filter(7);
     image blur = convolve_image(im, f, 1);
@@ -232,42 +255,46 @@ void test_convolution(){
     free_image(gt);
 }
 
-void test_gaussian_filter(){
+void test_gaussian_filter()
+{
     image f = make_gaussian_filter(7);
     int i;
 
-    for(i = 0; i < f.w * f.h * f.c; i++){
+    for (i = 0; i < f.w * f.h * f.c; i++)
+    {
         f.data[i] *= 100;
     }
 
     image gt = load_image("figs/gaussian_filter_7.png");
-    TEST(same_image(f, gt));    
+    TEST(same_image(f, gt));
     free_image(f);
     free_image(gt);
 }
 
-void test_gaussian_blur(){
+void test_gaussian_blur()
+{
     image im = load_image("data/dog.jpg");
     image f = make_gaussian_filter(2);
     image blur = convolve_image(im, f, 1);
     clamp_image(blur);
 
     image gt = load_image("figs/dog-gauss2.png");
-    TEST(same_image(blur, gt));    
+    TEST(same_image(blur, gt));
     free_image(im);
     free_image(f);
     free_image(blur);
     free_image(gt);
 }
 
-void test_hybrid_image(){
+void test_hybrid_image()
+{
     image man = load_image("data/melisa.png");
     image woman = load_image("data/aria.png");
     image f = make_gaussian_filter(2);
     image lfreq_man = convolve_image(man, f, 1);
     image lfreq_w = convolve_image(woman, f, 1);
-    image hfreq_w = sub_image(woman , lfreq_w);
-    image reconstruct = add_image(lfreq_man , hfreq_w);
+    image hfreq_w = sub_image(woman, lfreq_w);
+    image reconstruct = add_image(lfreq_man, hfreq_w);
     image gt = load_image("figs/hybrid.png");
     clamp_image(reconstruct);
     TEST(same_image(reconstruct, gt));
@@ -281,12 +308,13 @@ void test_hybrid_image(){
     free_image(gt);
 }
 
-void test_frequency_image(){
+void test_frequency_image()
+{
     image im = load_image("data/dog.jpg");
     image f = make_gaussian_filter(2);
     image lfreq = convolve_image(im, f, 1);
     image hfreq = sub_image(im, lfreq);
-    image reconstruct = add_image(lfreq , hfreq);
+    image reconstruct = add_image(lfreq, hfreq);
 
     image low_freq = load_image("figs/low-frequency.png");
     image high_freq = load_image("figs/high-frequency-clamp.png");
@@ -305,7 +333,8 @@ void test_frequency_image(){
     free_image(high_freq);
 }
 
-void test_sobel(){
+void test_sobel()
+{
     image im = load_image("data/dog.jpg");
     image *res = sobel_image(im);
     image mag = res[0];
@@ -318,16 +347,20 @@ void test_sobel(){
     TEST(gt_mag.w == mag.w && gt_theta.w == theta.w);
     TEST(gt_mag.h == mag.h && gt_theta.h == theta.h);
     TEST(gt_mag.c == mag.c && gt_theta.c == theta.c);
-    if( gt_mag.w != mag.w || gt_theta.w != theta.w || 
-        gt_mag.h != mag.h || gt_theta.h != theta.h || 
-        gt_mag.c != mag.c || gt_theta.c != theta.c ) return;
+    if (gt_mag.w != mag.w || gt_theta.w != theta.w ||
+        gt_mag.h != mag.h || gt_theta.h != theta.h ||
+        gt_mag.c != mag.c || gt_theta.c != theta.c)
+        return;
     int i;
-    for(i = 0; i < gt_mag.w*gt_mag.h; ++i){
-        if(within_eps(gt_mag.data[i], 0)){
+    for (i = 0; i < gt_mag.w * gt_mag.h; ++i)
+    {
+        if (within_eps(gt_mag.data[i], 0))
+        {
             gt_theta.data[i] = 0;
             theta.data[i] = 0;
         }
-        if(within_eps(gt_theta.data[i], 0) || within_eps(gt_theta.data[i], 1)){
+        if (within_eps(gt_theta.data[i], 0) || within_eps(gt_theta.data[i], 1))
+        {
             gt_theta.data[i] = 0;
             theta.data[i] = 0;
         }
@@ -342,8 +375,6 @@ void test_sobel(){
     free_image(gt_theta);
     free(res);
 }
-
-
 
 int do_test()
 {
@@ -373,6 +404,5 @@ void run_tests()
     test_hybrid_image();
     test_frequency_image();
     test_sobel();
-    printf("%d tests, %d passed, %d failed\n", tests_total, tests_total-tests_fail, tests_fail);
+    printf("%d tests, %d passed, %d failed\n", tests_total, tests_total - tests_fail, tests_fail);
 }
-
